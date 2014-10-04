b0289bf1f5209c772b03204184591ee859846e57
Leo Tanaka <tabbo@tfwno.gf>
2014-10-04 01:51:52 -0500

    Fixed some stuff

--- javascripts/misc.js
+++ javascripts/misc.js
@@ -1,54 +1,92 @@
-$('#1').hover(function() { 
-	$('#1full').toggle(650);
+$('#1').click(function() { 
+	//$('#1full').toggle(650);
+	$('#1full').slideToggle(650);
+	$('#1full').css('display', 'block');
+        $('#1full').siblings().hide('slow');
 });
-$('#2').hover(function() { 
-	$('#2full').toggle(500); 
+$('#2').click(function() { 
+	//$('#2full').toggle(500); 
+	$('#2full').slideToggle(650);
+	$('#2full').css('display', 'block');
+        $('#2full').siblings().hide('slow');
 });
-$('#3').hover(function() { 
-	$('#3full').toggle(500); 
+$('#3').click(function() { 
+	$('#3full').slideToggle(650);
+	$('#3full').css('display', 'block');
+        $('#3full').siblings().hide('slow');
 });
-$('#4').hover(function() { 
-	$('#4full').toggle(500); 
+$('#4').click(function() { 
+	$('#4full').slideToggle(650);
+	$('#4full').css('display', 'block');
+        $('#4full').siblings().hide('slow');
 });
-$('#5').hover(function() { 
-	$('#5full').toggle(500); 
+$('#5').click(function() { 
+	$('#5full').slideToggle(650);
+	$('#5full').css('display', 'block');
+        $('#5full').siblings().hide('slow');
 });
-$('#6').hover(function() { 
-	$('#6full').toggle(500); 
+$('#6').click(function() { 
+	$('#6full').slideToggle(650);
+	$('#6full').css('display', 'block');
+        $('#6full').siblings().hide('slow');
 });
-$('#7').hover(function() { 
-	$('#7full').toggle(500); 
+$('#7').click(function() { 
+	$('#7full').slideToggle(650);
+	$('#7full').css('display', 'block');
+        $('#7full').siblings().hide('slow');
 });
-$('#8').hover(function() { 
-	$('#8full').toggle(500); 
+$('#8').click(function() { 
+	$('#8full').slideToggle(650);
+	$('#8full').css('display', 'block');
+        $('#8full').siblings().hide('slow');
 });
-$('#9').hover(function() { 
-	$('#9full').toggle(500); 
+$('#9').click(function() { 
+	$('#9full').slideToggle(650);
+	$('#9full').css('display', 'block');
+        $('#9full').siblings().hide('slow');
 });
-$('#10').hover(function() { 
-	$('#10full').toggle(500); 
+$('#10').click(function() { 
+	$('#10full').slideToggle(650);
+	$('#10full').css('display', 'block');
+        $('#10full').siblings().hide('slow');
 });
-$('#11').hover(function() { 
-	$('#11full').toggle(500); 
+$('#11').click(function() { 
+	$('#11full').slideToggle(650);
+	$('#11full').css('display', 'block');
+        $('#11full').siblings().hide('slow');
 });
-$('#12').hover(function() { 
-	$('#12full').toggle(500); 
+$('#12').click(function() { 
+	$('#12full').slideToggle(650);
+	$('#12full').css('display', 'block');
+        $('#12full').siblings().hide('slow');
 });
-$('#13').hover(function() { 
-	$('#13full').toggle(500); 
+$('#13').click(function() { 
+	$('#13full').slideToggle(650);
+	$('#13full').css('display', 'block');
+        $('#13full').siblings().hide('slow');
 });
-$('#14').hover(function() { 
-	$('#14full').toggle(500); 
+$('#14').click(function() { 
+	$('#14full').slideToggle(650);
+	$('#14full').css('display', 'block');
+        $('#14full').siblings().hide('slow');
 });
-$('#15').hover(function() { 
-	$('#15full').toggle(500); 
+$('#15').click(function() { 
+	$('#15full').slideToggle(650);
+	$('#15full').css('display', 'block');
+        $('#15full').siblings().hide('slow');
 });
-$('#16').hover(function() { 
-	$('#16full').toggle(500); 
+$('#16').click(function() { 
+	$('#16full').slideToggle(650);
+	$('#16full').css('display', 'block');
+        $('#16full').siblings().hide('slow');
 });
-$('#17').hover(function() { 
-	$('#17full').toggle(500); 
+$('#17').click(function() { 
+	$('#17full').slideToggle(650);
+	$('#17full').css('display', 'block');
+        $('#17full').siblings().hide('slow');
 });
-$('#18').hover(function() { 
-	$('#18full').toggle(500); 
+$('#18').click(function() { 
+	$('#18full').slideToggle(650);
+	$('#18full').css('display', 'block');
+        $('#18full').siblings().hide('slow');
 });
